06c085588dc2a412c45fc016a42b310d7b2027ce
sin <swsinswsin@gmail.com>
2019-01-11 17:07:24 +0900

    11JAN2019

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,22 @@
-export default () => {
-    return undefined;
+import Axios from "axios";
+
+//Listen again lectue 170
+export default (state, action) => {
+    //bad!
+    // return document.querySelector('input')
+
+    //bad!
+    // return Axios.get('./posts');
+
+    //good
+    // return state + action;
+
+    //bad!
+    // state[0] = 'Sam'
+    // state.pop()
+    // state.push()
+
+    //bad!
+    // state.name = 'Same'
+    // state.age = 30;
 };
